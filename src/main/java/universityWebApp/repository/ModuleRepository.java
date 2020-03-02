c506af30b7f8d9bb6924ac5b90fd2b2c7a4054fa
charliekelly13 <charleskelly13@gmail.com>
2020-03-02 17:12:44 +0000

    Added insert queries for current tables

--- src/main/java/universityWebApp/repository/ModuleRepository.java
+++ src/main/java/universityWebApp/repository/ModuleRepository.java
@@ -1,8 +1,13 @@
 package universityWebApp.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import universityWebApp.model.Module;
 
 @Repository
-public interface ModuleRepository extends JpaRepository<Module, String> { }
+public interface ModuleRepository extends JpaRepository<Module, String> {
+    @Query("INSERT INTO modules(id, module_name, module_description, coordinator_name, enrolled_students, maximum_students), VALUES(?1,?2,?3,?4,?4,?5,?6)")
+    void insertModule(String id, String moduleName, String moduleDescription, String coordinatorName, String enrolledStudents, String maximumStudents);
+
+}
