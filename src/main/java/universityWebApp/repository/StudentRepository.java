851421e7084e4e801d23e443cd0b2177f6b3c067
charliekelly13 <charleskelly13@gmail.com>
2020-03-03 21:16:35 +0000

    Implemented query to retrieve password

--- src/main/java/universityWebApp/repository/StudentRepository.java
+++ src/main/java/universityWebApp/repository/StudentRepository.java
@@ -1,9 +1,15 @@
 package universityWebApp.repository;
 
+import org.springframework.data.jpa.repository.Query;
 import universityWebApp.model.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface StudentRepository extends JpaRepository<Student, String> {}
+public interface StudentRepository extends JpaRepository<Student, String> {
+    @Query("select a.password from Student a where a.username = ?1")
+    List<String> findPasswordByUsername(String username);
+}
 
