b2f5353a546b18d2fdf926ed6fdcd102d715d909
charliekelly13 <charleskelly13@gmail.com>
2020-03-05 11:30:24 +0000

    Added functionality to get distributions for staff but currently commented out because unsure of where this should be placed

--- src/main/java/universityWebApp/controller/StudentController.java
+++ src/main/java/universityWebApp/controller/StudentController.java
@@ -5,13 +5,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
-import universityWebApp.exception.StudentNotFoundException;
-import universityWebApp.model.Module;
+import universityWebApp.model.Staff;
 import universityWebApp.model.Student;
-import universityWebApp.repository.EnrollmentRepository;
-import universityWebApp.repository.GradesRepository;
-import universityWebApp.repository.ModuleRepository;
-import universityWebApp.repository.StudentRepository;
+import universityWebApp.repository.*;
 
 import java.util.HashMap;
 import java.util.List;
@@ -30,11 +26,14 @@ public class StudentController {
 
     @Autowired
     GradesRepository gradesRepository;
+
+    @Autowired
+    StaffRepository staffRepository;
     /**
      * This endpoint returns all distribution of gender
      */
     @RequestMapping(value = "student", method = RequestMethod.GET)
-    public HashMap getGenderDistribtuion() {
+    public HashMap getGenderDistribtuionStudent() {
 
         List<Student> students = studentRepository.findAll();
         HashMap<String, Integer> genderDis = new HashMap();
@@ -61,7 +60,7 @@ public class StudentController {
      * This endpoint returns all distribution of student nationality
      */
     @RequestMapping(value = "student", method = RequestMethod.GET)
-    public HashMap getNationalityDistribution() {
+    public HashMap getNationalityDistributionStudent() {
         List<Student> students = studentRepository.findAll();
         HashMap<String, Integer> nationalDis = new HashMap();
         String nat;
@@ -76,4 +75,52 @@ public class StudentController {
         }
         return nationalDis;
     }
+
+//    /**
+//     * This endpoint returns all distribution of gender for staff
+//     */
+//    @RequestMapping(value = "staff", method = RequestMethod.GET)
+//    public HashMap getGenderDistribtuionStaff() {
+//
+//        List<Staff> staff = staffRepository.findAll();
+//        HashMap<String, Integer> genderDis = new HashMap();
+//        genderDis.put("Male", 0);
+//        genderDis.put("Female", 0);
+//        genderDis.put("Other", 0);
+//        String gen;
+//        for(Staff s: staff){
+//            gen=s.getGender();
+//            if(gen.equals("Male")){
+//                genderDis.put("Male", genderDis.get("Male")+1);
+//            }
+//            else if(gen.equals("Female")){
+//                genderDis.put("Female", genderDis.get("Female")+1);
+//            }
+//            else if(gen.equals("Other")){
+//                genderDis.put("Other", genderDis.get("Other")+1);
+//            }
+//        }
+//        return genderDis;
+//    }
+//
+//    /**
+//     * This endpoint returns all distribution of staff nationality
+//     */
+//    @RequestMapping(value = "staff", method = RequestMethod.GET)
+//    public HashMap getNationalityDistributionStaff() {
+//        List<Staff> staff = staffRepository.findAll();
+//        HashMap<String, Integer> nationalDis = new HashMap();
+//        String nat;
+//        for(Staff s: staff){
+//            nat=s.getNationality();
+//            if(nationalDis.containsKey(nat)){
+//                nationalDis.put(nat, nationalDis.get(nat)+1);
+//            }
+//            else{
+//                nationalDis.put(nat, 0);;
+//            }
+//        }
+//        return nationalDis;
+//    }
+
 }
--- src/main/java/universityWebApp/model/Staff.java
+++ src/main/java/universityWebApp/model/Staff.java
@@ -1,4 +1,99 @@
 package universityWebApp.model;
 
+import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 public class Staff {
+    @Id
+    private String id;
+
+    @NotBlank
+    private String name;
+
+    @NotBlank
+    private String gender;
+
+    @NotBlank
+    private String nationality;
+
+//    @NotBlank
+//    private String address;
+//
+//    @NotBlank
+//    private String phoneNumber;
+//
+//    @NotBlank
+//    private String emailAddress;
+
+    public Staff() {
+        super();
+    }
+
+    public Staff(String id, String name, String gender) {
+        super();
+        this.id = id;
+        this.name = name;
+        this.gender = gender;
+//        this.address = address;
+//        this.phoneNumber = phoneNumber;
+//        this.emailAddress = emailAddress;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getNationality() {
+        return nationality;
+    }
+
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+
+//    public String getAddress() {
+//        return address;
+//    }
+//
+//    public void setAddress(String address) {
+//        this.address = address;
+//    }
+//
+//    public String getPhoneNumber() {
+//        return phoneNumber;
+//    }
+//
+//    public void setPhoneNumber(String phoneNumber) {
+//        this.phoneNumber = phoneNumber;
+//    }
+//
+//    public String getEmailAddress() {
+//        return emailAddress;
+//    }
+//
+//    public void setEmailAddress(String emailAddress) {
+//        this.emailAddress = emailAddress;
+//    }
+
 }
