3a7d29af0179b44d3ccc915d9af5ef82529042e8
charliekelly13 <charleskelly13@gmail.com>
2020-05-13 16:45:24 +0100

    Implementing listerners and services

---
+++ src/main/java/universityWebApp/service/AuthenticationFailureListener.java
@@ -0,0 +1,21 @@
+package universityWebApp.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationListener;
+import org.springframework.security.authentication.event.AuthenticationFailureBadCredentialsEvent;
+import org.springframework.security.web.authentication.WebAuthenticationDetails;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AuthenticationFailureListener implements ApplicationListener<AuthenticationFailureBadCredentialsEvent> {
+
+    @Autowired
+    private LoginAttemptService loginAttemptService;
+
+    public void onApplicationEvent(AuthenticationFailureBadCredentialsEvent e) {
+        WebAuthenticationDetails auth = (WebAuthenticationDetails)
+                e.getAuthentication().getDetails();
+
+        loginAttemptService.loginFailed(auth.getRemoteAddress());
+    }
+}
