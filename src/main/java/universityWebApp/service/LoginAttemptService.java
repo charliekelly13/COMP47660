70d6f3355f0842931238a995adb1b7a035de1e0c
charliekelly13 <charleskelly13@gmail.com>
2020-05-13 20:53:28 +0100

    Jpa error

--- src/main/java/universityWebApp/service/LoginAttemptService.java
+++ src/main/java/universityWebApp/service/LoginAttemptService.java
@@ -14,42 +14,62 @@ public class LoginAttemptService {
 
     public LoginAttemptService() {
         super();
+        System.out.println("1");
     }
 
     public void loginSucceeded(String key) {
+        System.out.println("2");
         Blacklist black;
+        System.out.println("3");
         try {
+            System.out.println("4");
             black = blacklistRepository.findById(key).orElseThrow(IpNotFoundException::new);
+            System.out.println("5");
             black.setAttempts(0);
+            System.out.println("6");
         }
         catch(IpNotFoundException i){
+            System.out.println("7");
             black = new Blacklist(key, 0);
+            System.out.println("8");
             blacklistRepository.save(black);
-
+            System.out.println("9");
         }
     }
 
     public void loginFailed(String key) {
+        System.out.println("10");
         int attempts = 0;
+        System.out.println("11");
         Blacklist black;
+        System.out.println("12");
         try{
+            System.out.println("13");
             black =  blacklistRepository.findById(key).orElseThrow(IpNotFoundException::new);
+            System.out.println("14");
             attempts =black.getAttempts();
+            System.out.println("15");
             black.setAttempts(attempts++);
+            System.out.println("16");
         }
         catch(IpNotFoundException i) {
+            System.out.println("17");
             black = new Blacklist(key, 1);
+            System.out.println("18");
             blacklistRepository.save(black);
+            System.out.println("19");
         }
 
     }
 
     public boolean isBlocked(String key) {
         try {
+            System.out.println("20");
             Blacklist black =  blacklistRepository.findById(key).orElseThrow(IpNotFoundException::new);
-            int MAX_ATTEMPT = 3;
-            return black.getAttempts() >= MAX_ATTEMPT;
+            System.out.println("21");
+            return black.getAttempts() >= 3;
         } catch (IpNotFoundException e) {
+            System.out.println("22");
             return false;
         }
     }
